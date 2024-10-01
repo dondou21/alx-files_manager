85bc6a04a4772d10656ab576ec389aa8827e5bde
dondou21 <dondouabiyi@gmail.com>
2024-10-01 10:25:02 +0200

    update

--- tests/controllers/AuthController.test.js
+++ tests/controllers/AuthController.test.js
@@ -150,4 +150,19 @@ describe('+ AuthController', () => {
         });
     });
   });
-});
+})t('+ Setting and getting an expired value', async function () {
+	    await redisClient.set('test_key', 356, 1);
+	    setTimeout(async () => {
+		          expect(await redisClient.get('test_key')).to.not.equal('356');
+		        }, 2000);
+	  });
+
+  it('+ Setting and getting a deleted value', async function () {
+	      await redisClient.set('test_key', 345, 10);
+	      await redisClient.del('test_key');
+	      setTimeout(async () => {
+		            console.log('del: test_key ->', await redisClient.get('test_key'));
+		            expect(await redisClient.get('test_key')).to.be.null;
+		          }, 2000);
+	    });
+});;
